85674a02693b2b12d86ea42029168b7a213b6423
Aurélien Ooms <aurelien.ooms@gmail.com>
2019-11-18 22:27:41 +0100

    :recycle: refactor: Move some helper functions to grid module.

--- filter.py
+++ filter.py
@@ -1,6 +1,8 @@
 from itertools import chain
 from grid import is_connected
-from grid import _neighbors_from_direction
+from grid import boundary
+from grid import corners
+from grid import _boundary_lengths
 from polyomino import mino_key
 from debug import debug
 
@@ -108,85 +110,6 @@ def _filter_with_odd_side_lengths ( minos ):
 
 def filter_with_odd_side_lengths ( minos ):
     return list(_filter_with_odd_side_lengths(minos))
-
-def boundary ( mino ):
-
-    """
-        Returns boundary of the input polyomino (clockwise).
-    """
-
-    if len(mino) == 0: return []
-
-    if len(mino) == 1: return [(0,0), (0,1), (1,1), (1,0)]
-
-    # xxxxxx
-    # ^ x
-    # ! xxxx
-    #   xxx
-    #  xx
-
-    n = len(mino)
-
-    first = mino.origin
-
-    debug("first", first)
-
-    boundary = []
-
-    # those are coordinates of the lattice
-
-    previous = first
-    if (first[0],first[1]+1) in mino:
-        boundary.append((first[0],first[1]+1))
-        second = (first[0],first[1]+1)
-    else:
-        boundary.append((first[0]+1,first[1]+1))
-        second = (first[0]+1,first[1])
-
-    current = second
-
-    while True:
-
-        assert len(boundary) <= 2 * (n+1)
-
-        debug('boundary', boundary)
-        debug('previous', previous)
-        debug('current', current)
-
-        for case, neighbor in enumerate(_neighbors_from_direction(previous, current)):
-
-            debug('neighbor', case, 'is', neighbor)
-
-            if case >= 1:
-                x = boundary[-1][0] + neighbor[0] - current[0]
-                y = boundary[-1][1] + neighbor[1] - current[1]
-                boundary.append((x,y))
-
-            if neighbor in mino:
-                previous = current
-                current = neighbor
-                break
-
-        if current == second and previous == first: break
-
-    boundary.pop()
-
-    return boundary
-
-def _corners ( b ) :
-
-    for p, q, r in zip(b[-1:] + b[:-1], b, b[1:] + b[:1]):
-        if (not p[0] == q[0] == r[0]) and (not p[1] == q[1] == r[1]):
-            yield q
-
-
-def corners ( b ) :
-    return list(_corners(b))
-
-def _boundary_lengths ( c ) :
-
-    for p, q in zip(c, c[1:] + c[:1]):
-        yield max(abs(p[0]-q[0]), abs(p[1]-q[1]))
 
 filters = {
     'one-sided': _filter_one_sided,
--- grid.py
+++ grid.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from debug import debug
 
 def neighbors(cell):
 
@@ -106,3 +107,83 @@ def boundary_cells ( mino ):
     killed = map(lambda t: t[0], filter(lambda t: t[1] == 4, kill_count.items()))
 
     return mino.difference(killed)
+
+
+def boundary ( mino ):
+
+    """
+        Returns boundary of the input polyomino (clockwise).
+    """
+
+    if len(mino) == 0: return []
+
+    if len(mino) == 1: return [(0,0), (0,1), (1,1), (1,0)]
+
+    # xxxxxx
+    # ^ x
+    # ! xxxx
+    #   xxx
+    #  xx
+
+    n = len(mino)
+
+    first = mino.origin
+
+    debug("first", first)
+
+    boundary = []
+
+    # those are coordinates of the lattice
+
+    previous = first
+    if (first[0],first[1]+1) in mino:
+        boundary.append((first[0],first[1]+1))
+        second = (first[0],first[1]+1)
+    else:
+        boundary.append((first[0]+1,first[1]+1))
+        second = (first[0]+1,first[1])
+
+    current = second
+
+    while True:
+
+        assert len(boundary) <= 2 * (n+1)
+
+        debug('boundary', boundary)
+        debug('previous', previous)
+        debug('current', current)
+
+        for case, neighbor in enumerate(_neighbors_from_direction(previous, current)):
+
+            debug('neighbor', case, 'is', neighbor)
+
+            if case >= 1:
+                x = boundary[-1][0] + neighbor[0] - current[0]
+                y = boundary[-1][1] + neighbor[1] - current[1]
+                boundary.append((x,y))
+
+            if neighbor in mino:
+                previous = current
+                current = neighbor
+                break
+
+        if current == second and previous == first: break
+
+    boundary.pop()
+
+    return boundary
+
+def _corners ( b ) :
+
+    for p, q, r in zip(b[-1:] + b[:-1], b, b[1:] + b[:1]):
+        if (not p[0] == q[0] == r[0]) and (not p[1] == q[1] == r[1]):
+            yield q
+
+
+def corners ( b ) :
+    return list(_corners(b))
+
+def _boundary_lengths ( c ) :
+
+    for p, q in zip(c, c[1:] + c[:1]):
+        yield max(abs(p[0]-q[0]), abs(p[1]-q[1]))
